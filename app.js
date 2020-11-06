0e1d336eca28cf7d267b608938e60f1b49718430
Luis Lozano <lslozano11@gmail.com>
2020-11-05 18:48:25 -0600

    Initial connect to MongoAtlas

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 
 require('dotenv').config();
 
-import express, { static } from "express";
-import { urlencoded } from "body-parser";
-import { lowerCase } from "lodash";
-import { mongoose } from "mongoose";
+const express     = require('express');
+const bodyParser  = require('body-parser');
+const lowerCase   = require('lodash');
+const mongoose    = require('mongoose');
+
+const port = process.env.PORT || 3000;
+const db = process.env.DB;
 
 const app = express();
 
-app.use(urlencoded({extended: true}));
-app.use(static("public"));
+// CONNECTION
+mongoose
+  .connect(db, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false
+  })
+  .then(x => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  })
+  .catch(err => {
+    console.error('Error connecting to mongo', err)
+  });
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -63,6 +80,6 @@ app.post("/compose", function(req, res){
   res.redirect("/");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(port, function() {
+  console.log(`Server started on port ${port}`);
 });
